6b6cdd567d8647187e28c18ea1b9dcfbcd200818
loevshootpeacemaker <101510223+loevshootpeacemaker@users.noreply.github.com>
2022-12-18 23:39:19 +0900

    2022-12-18 김택균 관리자 페이지 수정중

--- moumi/src/main/java/com/moumi/app/admin/memeberManage/MemberManageController.java
+++ moumi/src/main/java/com/moumi/app/admin/memeberManage/MemberManageController.java
@@ -1,5 +1,8 @@
 package com.moumi.app.admin.memeberManage;
 
+import org.springframework.stereotype.Controller;
+
+@Controller("admin.memberManageController")
 public class MemberManageController {
 
 }
