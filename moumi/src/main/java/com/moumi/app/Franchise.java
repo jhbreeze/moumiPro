45bf4d3205b51507b520cb7e44f2b30bbdf872b0
ksy2244 <rlathdus2244@naver.com>
2022-12-19 14:21:44 +0900

    myBatis DAO/Impl 관련 공통 기능 추가

--- moumi/src/main/java/com/moumi/app/Franchise.java
+++ moumi/src/main/java/com/moumi/app/Franchise.java
@@ -2,4 +2,32 @@ package com.moumi.app;
 
 public class Franchise {
 
+	private String name;
+	private String address;
+	private String item;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
 }
