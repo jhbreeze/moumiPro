a9f46876b8dd1f84424c5b970c9d64dd5385ff6c
seungcheol321 <instelec21@naver.com>
2022-12-20 14:10:26 +0900

    커뮤니티,공지사항 작업중

--- moumi/src/main/java/com/moumi/app/board/BoardController.java
+++ moumi/src/main/java/com/moumi/app/board/BoardController.java
@@ -1,5 +1,41 @@
 package com.moumi.app.board;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller("board.boardController")
+@RequestMapping("/board/*")
 public class BoardController {
 
+	
+	@RequestMapping(value = "list")
+	public String list(HttpServletRequest req,Model model) {
+		return ".board.list";
+	}
+	@GetMapping("write")
+	public String writeForm(Model model) {
+		model.addAttribute("mode","write");
+		
+		return ".board.write";
+	}
+	@PostMapping("write")
+	public String writeSubmit(Board dto,HttpSession session) throws Exception {
+		return "redirect:board/list";
+	}
+	
+	@GetMapping("article")
+	public String article(HttpSession session, Model model) {
+		return ".board.article";
+	}
+	
+
+	
+	
 }
