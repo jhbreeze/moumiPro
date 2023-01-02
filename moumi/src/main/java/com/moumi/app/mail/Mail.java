7e81a1bb73342ea67478cdaff21d33b724b4c7c6
ksy2244 <rlathdus2244@naver.com>
2023-01-02 14:30:57 +0900

    회원가입 성공 메일 발송 기능 추가

---
+++ moumi/src/main/java/com/moumi/app/mail/Mail.java
@@ -0,0 +1,75 @@
+package com.moumi.app.mail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class Mail {
+	private String receiverEmail;
+	private String senderEmail;
+	private String senderName;
+	private String subject;
+	private String content;
+
+	// 스프링에서 파일 받기
+	private List<MultipartFile> selectFile; // <input type="file" name="selectFile"
+	private List<String> savePathname = new ArrayList<>();
+
+	public String getReceiverEmail() {
+		return receiverEmail;
+	}
+
+	public void setReceiverEmail(String receiverEmail) {
+		this.receiverEmail = receiverEmail;
+	}
+
+	public String getSenderEmail() {
+		return senderEmail;
+	}
+
+	public void setSenderEmail(String senderEmail) {
+		this.senderEmail = senderEmail;
+	}
+
+	public String getSenderName() {
+		return senderName;
+	}
+
+	public void setSenderName(String senderName) {
+		this.senderName = senderName;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public List<MultipartFile> getSelectFile() {
+		return selectFile;
+	}
+
+	public void setSelectFile(List<MultipartFile> selectFile) {
+		this.selectFile = selectFile;
+	}
+
+	public List<String> getSavePathname() {
+		return savePathname;
+	}
+
+	public void setSavePathname(List<String> savePathname) {
+		this.savePathname = savePathname;
+	}
+
+}
--- moumi/src/main/java/com/moumi/app/member/MemberServiceImpl.java
+++ moumi/src/main/java/com/moumi/app/member/MemberServiceImpl.java
@@ -7,21 +7,26 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.moumi.app.common.dao.CommonDAO;
+import com.moumi.app.mail.Mail;
+import com.moumi.app.mail.MailSender;
 
 @Service("member.memberService")
 public class MemberServiceImpl implements MemberService {
 	@Autowired
 	private CommonDAO dao;
-	
+
 	@Autowired
 	private BCryptPasswordEncoder bcrypt;
-	
+
+	@Autowired
+	private MailSender mailSender;
+
 	@Override
 	public void insertMember(Member dto) throws Exception {
 		try {
 			long memberSeq = dao.selectOne("member.memberSeq");
 			dto.setUserCode(memberSeq);
-			
+
 			// 패스워드 암호화
 			String encPassword = bcrypt.encode(dto.getPwd());
 			dto.setPwd(encPassword);
@@ -29,17 +34,32 @@ public class MemberServiceImpl implements MemberService {
 			dto.setEnabled(1);
 			// 회원정보 저장
 			dao.insertData("member.insertMember", dto);
-			
+
 			// 권한 저장
 			dto.setAuthority("ROLE_USER");
 			dao.insertData("member.insertAuthority", dto);
-			
+
+			String result;
+			result = dto.getUserName() + "님, 회원가입 성공! <b>";
+			Mail mail = new Mail();
+			mail.setReceiverEmail(dto.getEmail());
+
+			mail.setSenderEmail("@naver.com");
+			mail.setSenderName("MOUMI 관리자");
+			mail.setSubject("회원가입 성공 메일");
+			mail.setContent(result);
+
+			boolean b = mailSender.mailSend(mail);
+			if (!b) {
+				throw new Exception("이메일 전송중 오류가 발생했습니다.");
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw e;
 		}
 	}
-	
+
 	@Override
 	public Member readMember(String email) {
 		Member dto = null;
@@ -47,29 +67,27 @@ public class MemberServiceImpl implements MemberService {
 		try {
 			dto = dao.selectOne("member.readMember", email);
 
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		return dto;
 	}
-	
+
 	@Override
 	public Member readMember1(String nickName) {
 		Member dto = null;
-		
+
 		try {
 			dto = dao.selectOne("member.readMember1", nickName);
-			
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return dto;
 	}
-	
+
 	@Override
 	public Member readMember(long userCode) {
 		Member dto = null;
@@ -85,7 +103,7 @@ public class MemberServiceImpl implements MemberService {
 	}
 
 //	updateUserType
-	
+
 	@Override
 	public void updateLastLogin(String email) throws Exception {
 		try {
@@ -99,12 +117,12 @@ public class MemberServiceImpl implements MemberService {
 	@Override
 	public void updateMember(Member dto) throws Exception {
 		try {
-			boolean bPwdUpdate = ! isPasswordCheck(dto.getEmail(), dto.getPwd());
-			if( bPwdUpdate ) {
+			boolean bPwdUpdate = !isPasswordCheck(dto.getEmail(), dto.getPwd());
+			if (bPwdUpdate) {
 				// 패스워드가 변경된 경우에만 member1 수정
 				String encPassword = bcrypt.encode(dto.getPwd());
 				dto.setPwd(encPassword);
-				
+
 				dao.updateData("member.updateMember1", dto);
 			}
 
@@ -113,22 +131,21 @@ public class MemberServiceImpl implements MemberService {
 			e.printStackTrace();
 			throw e;
 		}
-		
+
 	}
 
 //	deleteMember
-	
-//	generatePwd
 
+//	generatePwd
 
 	@Override
 	public boolean isPasswordCheck(String email, String pwd) {
 		Member dto = readMember(email);
-		
-		if(dto == null) {
+
+		if (dto == null) {
 			return false;
 		}
-		
+
 		return bcrypt.matches(email, dto.getPwd());
 	}
 
@@ -136,35 +153,34 @@ public class MemberServiceImpl implements MemberService {
 	@Override
 	public void updatePwd(Member dto) throws Exception {
 		try {
-			
-			if(isPasswordCheck(dto.getEmail(), dto.getPwd())) {
-				throw new RuntimeException("패스워드가 기존 패스워드와 일치 합니다."); 
+
+			if (isPasswordCheck(dto.getEmail(), dto.getPwd())) {
+				throw new RuntimeException("패스워드가 기존 패스워드와 일치 합니다.");
 			}
-			
+
 			String encPassword = bcrypt.encode(dto.getPwd());
 			dto.setPwd(encPassword);
-			
+
 			dao.updateData("member.updateMember1", dto);
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
-			
+
 			throw e;
 		}
-		
+
 	}
-	
-	
+
 	@Override
 	public int checkFailureCount(String email) {
 		int result = 0;
-		
+
 		try {
 			result = dao.selectOne("member.checkFailureCount", email);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return result;
 	}
 
@@ -180,7 +196,7 @@ public class MemberServiceImpl implements MemberService {
 
 	@Override
 	public void updateMemberEnabled(Map<String, Object> map) throws Exception {
-		
+
 	}
 
 	@Override
@@ -195,23 +211,19 @@ public class MemberServiceImpl implements MemberService {
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw e;
-		}		
+		}
 	}
 
 	@Override
 	public Member readMyPage(String email) throws Exception {
 		Member dto = null;
 		try {
-			dto = dao.selectOne("member.readMember", email); 
+			dto = dao.selectOne("member.readMember", email);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return dto;
 	}
-
-
-
-
 
 }
